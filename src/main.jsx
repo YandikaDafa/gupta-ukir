a0a6c607435a727977321cdc032a9c9828d43ecd
Dafa <143941273+YandikaDafa@users.noreply.github.com>
2026-09-06 21:35:30 +0800

    Add new product routes in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,10 @@ import { HashRouter, Routes, Route } from 'react-router-dom'
 import App from './App'
 import TentangKami from './pages/TentangKami'
 import Produk from './pages/Produk'
+import ProdukRelief from './pages/ProdukRelief'
+import ProdukLoster from './pages/ProdukLoster';
+import ProdukOrnamen from './pages/ProdukOrnamen';
+import ProdukPatung from './pages/ProdukPatung';
 import './index.css' // Pastikan file CSS Anda ada
 
 createRoot(document.getElementById('root')).render(
@@ -12,6 +16,10 @@ createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/produk" element={<Produk />} />
+        <Route path="/produk-relief" element={<ProdukRelief />} />
+        <Route path="/produk-loster" element={<ProdukLoster />} />
+        <Route path="/produk-ornamen" element={<ProdukOrnamen />} />
+        <Route path="/produk-patung" element={<ProdukPatung />} />
         <Route path="/tentang" element={<TentangKami />} />
       </Routes>
     </HashRouter>
